0535c2de2e074cff28956a707813c12e0d5821a4
kolbisz <przemek.nowak333@protonmail.com>
2025-05-13 13:46:20 +0200

    Added basic logic for character password generation

---
+++ demo/src/main/java/com/chopaki/logic/Chars.java
@@ -0,0 +1,52 @@
+package com.chopaki.logic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+
+public class Chars {
+
+    private static Character[] convertToCharacterArray(char[] arr) {
+        if(arr == null){
+            return null;
+        }
+        Character[] finalArray = new Character[arr.length];
+        for(int i=0;i<arr.length;i++){
+            finalArray[i] = arr[i];
+        }
+        return finalArray;
+    }
+
+    public static String genChars(int length, Boolean nums, Boolean special, Boolean large){
+        Character[] alphabet = convertToCharacterArray(("abcdefghijklmnopqrstiwvxyz".toCharArray()));
+        Character[] digits = convertToCharacterArray(("1234567890".toCharArray()));
+        Character[] specials = convertToCharacterArray(("!@#$%^&*?+-".toCharArray()));
+        Character[] larges = convertToCharacterArray("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray());
+
+        Random generator = new Random();
+        String password = "";
+        List<Character> completeList = new ArrayList<>();
+
+        completeList.addAll(new ArrayList<>(Arrays.asList(alphabet)));
+
+        if(nums){
+            completeList.addAll(new ArrayList<>(Arrays.asList(digits)));
+        }
+        if(special){
+            completeList.addAll(new ArrayList<>(Arrays.asList(specials)));
+        }
+        if(large){
+            completeList.addAll(new ArrayList<>(Arrays.asList(larges)));
+        }
+
+        for(int i=0;i<length;i++){
+            Character character = completeList.get(generator.nextInt(completeList.size()));
+            password += character.toString();
+    }
+
+    return password;
+
+}
+}
--- demo/src/main/java/com/chopaki/passgen/Main.java
+++ demo/src/main/java/com/chopaki/passgen/Main.java
@@ -1,7 +1,9 @@
 package com.chopaki.passgen;
 
+import com.chopaki.logic.Chars;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Chars.genChars(19, true, true, true));
     }
 }
